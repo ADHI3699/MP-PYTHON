c047745be54a5640ac59f35f21ce069c5ece684d
ADHI3699 <78644991+ADHI3699@users.noreply.github.com>
2021-09-18 12:33:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+film = "enthiran"
+actor = "rejinikanth"
+actress = "aiswaryrai"
+director = "shanker"
+year_of_release = 2010
+print(f"my fav movie is {film},\n the lead actor and actress were {actor} and {actress},\n the film was realeased in {year_of_release}")
